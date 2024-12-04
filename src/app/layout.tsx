4618eabbeb086dd6270ed6505eb57dc160cf57be
aqsaaQaazi <aqsaakqaazi@gmail.com>
2024-12-05 03:24:54 +0500

    initial commit: setup project with tailwind and sanity

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Nexscribe | Today’s thoughts, Tomorrow's stories.",
+  description: " A blog that evolves with the times—offering reflections on the intersections of technology, culture, and the human condition.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
